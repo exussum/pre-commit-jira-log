46fd9658d1c91ef3b028aa2ed829af626e501388
spencer portée <spencer.portee@apploi.com>
2022-08-12 12:25:57 -0400

    First real commit

---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name="exussum-pre-commit-jira-log",
+    scripts=[
+        "scripts/populate_message",
+        "scripts/enforce_prefix_github",
+        "scripts/verify_message",
+    ],
+    version="1.0.0",
+    description="Pre-commit jira utilities",
+    author="spencer portée",
+    install_requires=["PyGithub==1.55", "docopt==0.6.2", "gitpython==3.1.27"],
+)
